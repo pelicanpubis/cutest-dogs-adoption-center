e41681be9afe41858fffb580fb37394628e62723
pelicanpubis <norah.binsalamah@hotmail.com>
2024-01-31 11:41:42 +0100

    add views

--- CutestDogsAdoptionService/Models/DogModel.cs
+++ CutestDogsAdoptionService/Models/DogModel.cs
@@ -2,5 +2,13 @@
 {
     public class DogModel
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Cuteness { get; set; }
+        public string Image { get; set; }
+        public string FavFood { get; set; }
+        public string FavToy { get; set; }
+        public int Temperament { get; set; }
+        public bool IsAdopted { get; set; }
     }
 }
